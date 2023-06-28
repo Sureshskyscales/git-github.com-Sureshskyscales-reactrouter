a2a2cb8ead2a49548837b26e81cf7a66b4f66537
Suresh S <suresh@skyscales.com>
2023-06-28 18:09:25 +0530

    Home page update

---
+++ src/Pages/Footer.js
@@ -0,0 +1,12 @@
+import { Box, Typography } from '@mui/material'
+import React from 'react'
+
+const Footer = () => {
+  return (
+ 
+        <Typography> @Skyscales.Gmbh</Typography>
+
+  )
+}
+
+export default Footer
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,35 +1,127 @@
-import { Container,Box, Typography,  Fade } from '@mui/material';
-import { alignProperty } from '@mui/material/styles/cssUtils';
-import React, { useState, useEffect } from 'react';
-import Hero from "../Images/hero.jpg"
+import { Container, Grid, Typography, Fade, ThemeProvider, Box } from '@mui/material';
+import React, { useEffect, useRef } from 'react';
+import Hero from "../Images/hero.jpg";
+import { createTheme } from '@mui/material/styles';
+import { useInView } from 'react-intersection-observer';
 
+const theme = createTheme({
+  typography: {
+    fontFamily: 'Roboto, Arial, sans-serif',
+  },
+});
 
 const Home = () => {
-  const [opacity, setOpacity] = useState(0.5);
-  const [position,setPosition] = useState()
-  useEffect(() => {
-    // This code will execute once the component renders.
-    const timeout = setTimeout(() => {
-      setOpacity(1);
-    }, 2000);
+  const txtRef = useRef(null);
+  const boxRef = useRef(null);
 
-    return () => {
-      clearTimeout(timeout);
-    };
-  }, []);
+  const [txtInViewRef, txtInView] = useInView({
+    triggerOnce: true,
+  });
+  const [boxInViewRef, boxInView] = useInView({
+    triggerOnce: true,
+  });
+
+  useEffect(() => {
+    const txtElement = txtRef.current;
+    const boxElement = boxRef.current;
+
+    if (txtElement && txtInView) {
+      txtElement.style.opacity = '1';
+      txtElement.style.transform = 'translateX(0)';
+    }
+
+    if (boxElement && boxInView) {
+      boxElement.style.opacity = '1';
+      boxElement.style.transform = 'translateY(0)';
+    }
+  }, [txtInView, boxInView]);
 
   return (
-    <>
-    <Fade in={opacity !== 0} timeout={1000}>
-    <Box className='hero' style={{backgroundImage:`url(${Hero})`,opacity: `${opacity}`, height:'100vh', backgroundRepeat: 'no-repeat',  width:"auto", backgroundPosition:'center center', backgroundSize:"250vh 160vh", paddingTop:"10vh"}}>
-    </Box>
-    </Fade>
-    <Container sx={{paddingTop:"20vh", height: "100vh"}}>
-    <Typography variant="h3" align= "center">Let's Build Future Together</Typography>
-    <Typography variant="body1" align= "center">Everything starts with a small idea. At SkyScales, we turn your ideas into innovative products that builds a more fantastic future</Typography>
-    </Container>
-    </>
-  )
-}
+    <ThemeProvider theme={theme}>
+      <Fade in={true} timeout={2000}>
+        <Container sx={{ height: '100vh' }}>
+          <Grid
+            container
+            spacing={0}
+            alignItems="center"
+            justifyContent="center"
+            sx={{
+              backgroundImage: `url(${Hero})`,
+              backgroundRepeat: 'no-repeat',
+              backgroundPosition: 'center center',
+              backgroundSize: 'cover',
+              minHeight: '100vh',
+              marginLeft: '-10vw', // Adjust marginLeft to offset the space on the left side
+              width: '110vw',
+            }}
+            ref={boxInViewRef}
+          >
+            <Grid item xs={12} md={6}>
+              <Box
+                ref={txtInViewRef}
+                sx={{
+                  opacity: 0,
+                  transform: 'translateX(-100px)',
+                  transition: 'opacity 1s, transform 2s',
+                  '&.fade-in': {
+                    opacity: 1,
+                    transform: 'translateX(0)',
+                  },
+                  fontFamily: 'Roboto, Arial, sans-serif',
+                }}
+                className={txtInView ? 'fade-in' : ''}
+              >
+                <Typography variant='h1' justifySelf="center"> Hi </Typography>
+                <Typography variant='h4' justifySelf="center"> Welcome to Sky scales </Typography>
+              </Box>
+            </Grid>
+          </Grid>
 
-export default Home
+          <Container sx={{ paddingTop: '20vh', height: '100vh' }}>
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <Typography variant="h3" align="center">
+                  Let's Build Future Together
+                </Typography>
+                <Typography variant="body1" align="center">
+                  Everything starts with a small idea. At SkyScales, we turn your ideas into innovative products that build a more fantastic future.
+                </Typography>
+              </Grid>
+            </Grid>
+
+            <div ref={boxRef}>
+              <Grid
+                container
+                spacing={2}
+                sx={{
+                  marginTop: '40px',
+                  opacity: 0,
+                  transform: 'translateY(100px)',
+                  transition: 'opacity 1s, transform 1s',
+                  '&.slide-in': {
+                    opacity: 1,
+                    transform: 'translateY(0)',
+                  },
+                }}
+                className={boxInView ? 'slide-in' : ''}
+              >
+                <Grid item xs={12} md={6}>
+                  <Box sx={{ height: '200px', backgroundColor: '#e8e8e8' }}>
+                    <Typography> Box1</Typography>
+                  </Box>
+                </Grid>
+                <Grid item xs={12} md={6}>
+                  <Box sx={{ height: '200px', backgroundColor: '#f4f4f4' }}>
+                  <Typography> Box2</Typography>
+                  </Box>
+                </Grid>
+              </Grid>
+            </div>
+          </Container>
+        </Container>
+      </Fade>
+    </ThemeProvider>
+  );
+};
+
+export default Home;
